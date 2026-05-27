f10d6fe1c8abd8d85b92d78169db1329ddf65a57
Meareg Teame <mearegteame99995555@gmail.com>
2026-05-28 01:41:00 +0300

    done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcss from "tailwindcss"
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +13,6 @@ export default defineConfig({
       process: true,
     },
   })],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
